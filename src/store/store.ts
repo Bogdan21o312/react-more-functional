617b50c2dbd05ab81abf2512e70f947ac39cad0b
Bogdan21o312 <lutskiv2010@gmail.com>
2022-11-27 20:27:02 +0200

    Updated store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,20 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {testAPI} from "../sevices/test";
+import {albumsAPI} from "../sevices/AlbumsService";
+import {commentsAPI} from "../sevices/CommentsService";
+import {photosAPI} from "../sevices/PhotosService";
+import {postAPI} from "../sevices/PostService";
+import {todosAPI} from "../sevices/TodosService";
+import {usersAPI} from "../sevices/UsersService";
 
 const rootReducer = combineReducers({
-    [testAPI.reducerPath]: testAPI.reducer
+    [testAPI.reducerPath]: testAPI.reducer,
+    [albumsAPI.reducerPath]: albumsAPI.reducer,
+    [commentsAPI.reducerPath]: commentsAPI.reducer,
+    [photosAPI.reducerPath]: photosAPI.reducer,
+    [postAPI.reducerPath]: postAPI.reducer,
+    [todosAPI.reducerPath]: todosAPI.reducer,
+    [usersAPI.reducerPath]: usersAPI.reducer,
 })
 
 export const setupStore = () => {
