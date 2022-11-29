ae4116207608ce41bf989c2395530dc4df5bd363
Bogdan21o312 <lutskiv2010@gmail.com>
2022-11-29 19:11:20 +0200

    Created usePosts

---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,26 @@
+import {useMemo} from "react";
+import {IPost} from "../models/IPost";
+
+export const useStortedPosts = (posts: any, sort: any) => {
+
+    const sortedPosts = useMemo(() => {
+        if(sort) {
+            return [...posts].sort((a, b) => a[sort].localeCompare(b[sort]))
+        }
+        return posts;
+    }, [sort, posts])
+
+    return sortedPosts;
+
+}
+
+export const usePosts = (posts: any, sort: any, query: any) => {
+
+    const sortedPosts = useStortedPosts(posts, sort);
+
+    const sortedAndSearchedPosts = useMemo( ()  => {
+        return sortedPosts.filter((post: any) => post.title.includes(query))
+    }, [query, sortedPosts])
+
+    return sortedAndSearchedPosts;
+}
